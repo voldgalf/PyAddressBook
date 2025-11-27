60634dc1ff5a9e7baefb35b569c31d32e9ac7014
voldgalf <50calibursword@gmail.com>
2025-11-27 13:57:24 -0500

    Update __init__.py

--- sample/__init__.py
+++ sample/__init__.py
@@ -3,10 +3,9 @@ import os
 
 REGULAR_PROPERTIES = ["First Name", "Last Name", "Address", "Phone"]
 
-class BookContact:
+class AddressContact:
     
     def __init__(self, fullName):
-        """Class docstrings go here."""
         self.info = {}
         
         self.addInformation("First Name", fullName.split()[0])
@@ -29,42 +28,21 @@ class AddressBook:
     
     def __init__(self):
         
-        self.records: List[BookRecord] = []
+        self.contacts: List[BookContact] = []
     def getContacts(self):
-        return self.records
-    def addContact(self, BookRecord):
+        return self.contacts
+    def addContact(self, BookContact):
         
-        self.records.append(BookRecord)
+        self.contacts.append(BookContact)
         
     def load(self, filePath) -> bool:
         if os.path.exists(filePath):
 
             with open(filePath, 'rb') as f:
-                self.records = pickle.load(f)
+                self.contacts = pickle.load(f)
             return True;
         else:
             return False
-    def save(self, fileName):
-        with open(fileName, 'wb') as f:
-            pickle.dump(self.records, f)  
-            
-FileName = "myAddress.book"
-
-myAddressBook = AddressBook()
-
-if(not myAddressBook.load(FileName)):
-    print(f"{FileName} does not exist... creating new address book")
-    myAddressBook = AddressBook()
-    John = BookRecord("John McClane")
-    John.addInformation("Address", "New York")
-    John.addInformation("Phone", "123-456-7891")
-
-    myAddressBook.addRecord(John)
-
-    myAddressBook.save(FileName)
-
-    print(John.printAllInformation())
-else:
-    print(f"{FileName} Loaded")
-    for record in myAddressBook.getRecords():
-        record.printAllInformation()
+    def save(self, filePath):
+        with open(filePath, 'wb') as f:
+            pickle.dump(self.contacts, f)  
